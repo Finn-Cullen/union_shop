61f53f9c604900aa98fe2fe9e1f98c232477f8ff
finn cullen <finncullen83@gmail.com>
2025-11-20 02:47:39 +0000

    changed format so that the footer doesnt overflow

--- lib/the_print_shack.dart
+++ lib/the_print_shack.dart
@@ -25,56 +25,60 @@ class PrintPageState extends State<PrintPage> {
       perslineinpset();
     });
     return Scaffold(
-      body: Column(
-        children: [
-          const Navbar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Navbar(),
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
 
-                child: Image.network('https://shop.upsu.net/cdn/shop/products/Personalised_Image_1024x1024@2x.jpg?v=1562949869',fit: BoxFit.fill),
-              ),
-              SizedBox(
-                child: Column(
-                  children: [
-                    const Text('Personalisation'),
-                    Text('£' + persprice.toString() + '.00'),
-                    const Text('Tax included'),
-
-                    // select pers text type
-                    DropdownMenu<PersTypes>(
-                      hintText: persdesc,
-                      dropdownMenuEntries: PersTypes.values
-                        .map<DropdownMenuEntry<PersTypes>>(
-                          (PersTypes itm) {
-                            return DropdownMenuEntry<PersTypes>(
-                              value: itm,
-                              label: itm.perstype,
-                            );
-                        }).toList(),
-                      onSelected: (PersTypes? pers){
-                        setState(() {
-                          if(pers != null){
-                            persprice = pers.persprice;
-                            persdesc = pers.perstype;
-                          }
-                          perslineinpset();
-                        });
-                      },
-                    ),
-                    Column(children: perslineinp,), // bring this forward with sir, having textfields breaks everything and is an active bug in flutter
-
-                    ElevatedButton(onPressed: () => incrprod('Personalised shirt', persprice.toString(), 'https://shop.upsu.net/cdn/shop/products/Personalised_Image_1024x1024@2x.jpg?v=1562949869', context), child: const Text('add to cart'))
-                  ],
+                  child: Image.network('https://shop.upsu.net/cdn/shop/products/Personalised_Image_1024x1024@2x.jpg?v=1562949869',fit: BoxFit.fill),
                 ),
-              )
-            ],
-          ),
+                SizedBox(
+                  height: 600,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Personalisation'),
+                      Text('£' + persprice.toString() + '.00'),
+                      const Text('Tax included'),
 
-          const Footer(),
-        ],
+                      // select pers text type
+                      DropdownMenu<PersTypes>(
+                        hintText: persdesc,
+                        dropdownMenuEntries: PersTypes.values
+                          .map<DropdownMenuEntry<PersTypes>>(
+                            (PersTypes itm) {
+                              return DropdownMenuEntry<PersTypes>(
+                                value: itm,
+                                label: itm.perstype,
+                              );
+                          }).toList(),
+                        onSelected: (PersTypes? pers){
+                          setState(() {
+                            if(pers != null){
+                              persprice = pers.persprice;
+                              persdesc = pers.perstype;
+                            }
+                            perslineinpset();
+                          });
+                        },
+                      ),
+                      Column(children: perslineinp,), // bring this forward with sir, having textfields breaks everything and is an active bug in flutter
+
+                      ElevatedButton(onPressed: () => incrprod('Personalised shirt', persprice.toString(), 'https://shop.upsu.net/cdn/shop/products/Personalised_Image_1024x1024@2x.jpg?v=1562949869', context), child: const Text('add to cart'))
+                    ],
+                  ),
+                )
+              ],
+            ),
+
+            const Footer(),
+          ],
+        )
       )
     );
   }
