5542599569baa34dc0e01cfbaf930a96f530cc30
finn cullen <finncullen83@gmail.com>
2025-11-28 19:58:14 +0000

    reformatted print shack test

--- test/widgets_print_shack_test.dart
+++ test/widgets_print_shack_test.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
- 
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -20,7 +20,9 @@ void main() {
 
     await tester.pumpWidget(DefaultAssetBundle(
       bundle: bundle,
-      child: MaterialApp(home: Scaffold(body: SizedBox(width: 2000, child: const PrintPage()))),
+      child: MaterialApp(
+          home:
+              Scaffold(body: SizedBox(width: 2000, child: const PrintPage()))),
     ));
 
     await tester.pumpAndSettle();
@@ -40,7 +42,8 @@ class TestAssetBundle extends CachingAssetBundle {
   }
 
   @override
-  Future<T> loadStructuredBinaryData<T>(String key, FutureOr<T> Function(ByteData) loader) async {
+  Future<T> loadStructuredBinaryData<T>(
+      String key, FutureOr<T> Function(ByteData) loader) async {
     final codec = const StandardMessageCodec();
     final dynamic encoded = codec.encodeMessage(<String, List<String>>{});
     if (encoded is ByteData) return loader(encoded);
@@ -51,7 +54,11 @@ class TestAssetBundle extends CachingAssetBundle {
   @override
   Future<String> loadString(String key, {bool cache = true}) async {
     if (key.contains('PersTypes.json')) {
-      return jsonEncode({'values': [{'perstype': 'one line', 'persprice': '£3.00', 'lineoftext': 1}]});
+      return jsonEncode({
+        'values': [
+          {'perstype': 'one line', 'persprice': '£3.00', 'lineoftext': 1}
+        ]
+      });
     }
     return '';
   }
